f718570e6c050c5c9d102efa78fce848384f4fdd
Ryu Ruangchainikom <ryu.feelsgood@gmail.com>
2025-08-09 15:05:32 +0700

    god save us

--- simulation.py
+++ simulation.py
@@ -1,8 +1,10 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 # year setup
-start_year = 2050
-end_year = 2080
+start_year = 2025
+end_year = 2050
 years = list(range(start_year,end_year+1))
 
 # demographic setup
@@ -10,9 +12,33 @@ pop2025 = 71614788
 pop = pop2025
 TFR = 0.95
 
+poplist = []
+yearlist = []
+
 growth_rate = ((TFR/2.1)-1)/30
 
+migration_rate = 50000
+
 # simulation
-for x in range(years):
-    pop = pop * (1-growth_rate)
-    print(f"{2050+x}: {pop}")
+for x in range(end_year-start_year+1):
+    poplist.append(pop)
+    yearlist.append(2025+x)
+
+    print(f"{2025+x}: {pop}")
+    pop = round(pop * (1+growth_rate))
+    pop += migration_rate
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# plot data
+plt.plot(yearlist, poplist)
+
+# labels + title
+plt.xlabel('Years')
+plt.ylabel('Population')
+plt.title('Simulation')
+
+# display
+plt.show()
